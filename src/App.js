4c4c820efd9e58c123a5b653c7a0d761809414bd
vahid koohjani <vk1604@yahoo.com>
2023-08-27 21:42:02 +0200

    logout function add to SiteNav

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ import SiteFooter from './components/Common/SiteFooter';
 import SiteNav from './components/Common/SiteNav';
 import HomePage from './components/home/HomePage';
 import { Route, Routes } from 'react-router-dom';
-import LoginPage from './components/auth/LoginPage';
-import RegisterPage from './components/auth/RegisterPage';
+
 import Contacts from './components/contacts/Contacts';
 import awsExports from './aws-exports';
 import {Amplify} from 'aws-amplify';
@@ -21,12 +20,11 @@ function App() {
     {({signOut,user}) => (
     <div>  
     
-        <SiteNav/>
+        <SiteNav logOut={signOut}/>
         <Routes>
           <Route path='*' element={<HomePage/>}/>
           <Route path='/'  exact={true} element={<HomePage/>}/> 
-          <Route path='/login' element={<LoginPage/>}/>
-          <Route path='/register' element={<RegisterPage/>} />
+          
           <Route path='/contacts' element={<Contacts/>}/>
         </Routes>
         <SiteFooter/>
